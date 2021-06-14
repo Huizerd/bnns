f4a08a660453c5cc924c51e9fa9af3f680cd0039
tbekkering <m.c.bekkering@student.tudelft.nl>
2021-06-14 15:57:55 +0200

    threshold compare aangepast

--- vhdl/threshold.vhd
+++ vhdl/threshold.vhd
@@ -39,21 +39,24 @@ end threshold_bit;
 
 architecture Behavioral of threshold_bit is
 
-signal and_outputs : std_logic_vector(10 downto 0);
-signal or_outputs : std_logic_vector(9 downto 2);
+signal and_outputs_larger,and_outputs_smaller : std_logic_vector(10 downto 0);
+signal or_outputs_larger,or_outputs_smaller : std_logic_vector(9 downto 2);
 signal lastbitcheck :std_logic;
 signal SEL : std_logic_vector(3 downto 0);
-signal a,b,c,d, s1, s2, s3, s4, s5, s6, s7, s8 , a_and_b :std_logic;
+--signal a,b,c,d, s1, s2, s3, s4, s5, s6, s7, s8 , a_and_b :std_logic;
 begin
 
 GEN_AND: for I in 0 to 10 generate
-    and_outputs(I) <= popcount(I) and (not(threshold(I)));
+    and_outputs_larger(I) <= popcount(I) and (not(threshold(I)));
+    and_outputs_smaller(I) <= threshold(I) and (not(popcount(I)));
 end generate;
 
-or_outputs(9) <= and_outputs(10) or and_outputs(9);
+or_outputs_larger(9) <= and_outputs_larger(10) or and_outputs_larger(9);
+or_outputs_smaller(9) <= and_outputs_smaller(10) or and_outputs_smaller(9);
 
 GEN_OR: for I in 2 to 8 generate
-    or_outputs(I) <= and_outputs(I) or or_outputs(I+1);
+    or_outputs_larger(I) <= and_outputs_larger(I) or or_outputs_larger(I+1);
+    or_outputs_smaller(I) <= and_outputs_smaller(I) or or_outputs_smaller(I+1);
 end generate;
  
 --Check larger than upto last 2 bits
@@ -63,7 +66,7 @@ end generate;
 
 --Always one for 
 
-select_process : process (bias, popcount, threshold,or_outputs,lastbitcheck)
+select_process : process (bias, popcount, threshold,lastbitcheck,or_outputs_larger,and_outputs_smaller )
 begin
     SEL(3) <= popcount(1);
     SEL(2)  <= popcount(0);
@@ -80,11 +83,11 @@ begin
         when "1111" => lastbitcheck <= bias;
         when "0100" => lastbitcheck <= '1';
         when "1001" => lastbitcheck <= '1';
-        when "1110" => lastbitcheck <= '1';              
+        when "1101" => lastbitcheck <= '1';              
         when others => lastbitcheck <= '0';
     end case;
     
-    cell_value <= or_outputs(2) or (lastbitcheck and not or_outputs(2));
+    cell_value <= or_outputs_larger(2) or (not or_outputs_smaller(2) and (lastbitcheck and not or_outputs_larger(2)));
 end process;
 
 
